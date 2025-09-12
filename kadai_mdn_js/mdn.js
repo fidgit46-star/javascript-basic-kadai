43f4f277b4547b4b70ab9f5f2e3fe1558a8fa18e
fidgit46-star <fidgit46@gmail.com>
2025-09-12 18:14:45 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+//console.log(date);
+console.log(year +'年' + month + '月' + day + '日');
